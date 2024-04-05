52dc5805e23406ecaca1ec2edd9b3637040ea865
Chinh <bachinh456@gmail.com>
2024-04-05 20:32:42 +0700

    new comimt

--- src/routers/cart.js
+++ src/routers/cart.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { addItemCart, getCartByUserId } from "../controllers/cart";
+import {
+  addItemToCart,
+  decreaseProductQuantity,
+  getCartByUserId,
+  increaseProductQuantity,
+  removeFromCart,
+  updateProductQuantity,
+} from "../controllers/cart";
 
 const router = Router();
 
-router.post("/cart/add-to-cart", addItemCart);
-router.get("/cart/:userId", getCartByUserId);
+router.get("/carts/:userId", getCartByUserId);
+router.post("/carts/add-to-cart", addItemToCart);
+router.post("/carts/update", updateProductQuantity);
+router.post("/carts/remove", removeFromCart);
+router.post("/carts/increase", increaseProductQuantity);
+router.post("/carts/decrease", decreaseProductQuantity);
 
 export default router;
